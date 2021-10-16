9244a52e7773956d515dc31b35dd34029195193e
Chow Jing Lun <chowjl94@gmail.com>
2021-10-16 15:48:20 +0800

    add: Procfile

--- src/app.js
+++ src/app.js
@@ -1,17 +1,25 @@
 const express = require('express')
 const logger = require('morgan')
 const OpenApiValidator = require('express-openapi-validator');
+const swaggerUi = require('swagger-ui-express')
+const YAML = require('yamljs');
+const swaggerfile = YAML.load('./src/openapi/openapi.yml')
 
 module.exports = (router) => {
   const app = express()
   app.use(express.json())
   app.use(logger('common'))
+  app.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerfile))
+  app.use(router)
+  
   app.use(OpenApiValidator.middleware({
     apiSpec: './src/openapi/openapi.yml',
     validateApiSpec: true,
     validateRequests: true,
     validateResponses: true
   }))
+
+  
 
   app.use((err, req, res, next) => {
     // format error
@@ -21,6 +29,6 @@ module.exports = (router) => {
     });
   });
 
-  app.use(router)
+  
   return app
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,5 @@
 const express = require('express')
-const swaggerUi = require('swagger-ui-express')
-const YAML = require('yamljs');
-const swaggerfile = YAML.load('./src/openapi/openapi.yml')
+
 
 module.exports = (authMiddleware, authService,ampqservice, db) => {
   const router = express.Router()
@@ -14,7 +12,7 @@ module.exports = (authMiddleware, authService,ampqservice, db) => {
 
   //import auth with DI and pass authService
   router.use('/', require('./auth')(authService))
-  router.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerfile))//swagger ui to serve the specifications
+  // router.use('/api-docs',swaggerUi.serve,swaggerUi.setup(swaggerfile))//swagger ui to serve the specifications
 
   router.use(authMiddleware)
   router.use('/todo', require('./todo')(db,ampqservice))
